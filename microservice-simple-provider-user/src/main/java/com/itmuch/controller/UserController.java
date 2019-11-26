793b9277c4821016277ec61deb5b965cbf88500f
dingjinhui <676682693.qq.com>
2019-11-26 11:08:26 +0800

    feign传多个参数的几种方式
    map ，body ，parameter

--- microservice-simple-provider-user/src/main/java/com/itmuch/controller/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/controller/UserController.java
@@ -3,9 +3,9 @@ package com.itmuch.controller;
 import com.itmuch.dao.UserRepository;
 import com.itmuch.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 /**
  * @description: UserController
@@ -26,5 +26,21 @@ public class UserController {
     public User findById(@PathVariable Long id){
         System.out.println("port= 8000" );
         return userRepository.findOne(id);
+    }
+
+    @GetMapping("get0")
+    public User get0(User user){
+        return userRepository.findOne(user.getId());
+    }
+    @GetMapping("get1")
+    public User get1(@RequestParam HashMap map){
+        return userRepository.findOne(Long.parseLong(map.get("id").toString()));
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST,value = "post")
+    public User post(@RequestBody User user){
+        System.out.println("user = " + user);
+        return userRepository.findOne(user.getId());
     }
 }
