8b01ab0ee8057109ceeb7845d1847d7129a03b8a
dingjinhui <676682693.qq.com>
2019-12-06 21:18:46 +0800

    feign设置hystrix

---
+++ microservice-consumer-movie-feign-hystrix-fallback/src/main/java/com/itmuch/ConsumerMovieFeignHystrixFallbackApplication.java
@@ -0,0 +1,29 @@
+package com.itmuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description: ProviderUserApplication
+ * @author: ding
+ * @date: 2019/11/5 20:09
+ * @version: 1.0
+ */
+
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerMovieFeignHystrixFallbackApplication {
+
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieFeignHystrixFallbackApplication.class, args);
+    }
+}
