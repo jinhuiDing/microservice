bf37e682bd2789ebda0a4d20c54b5b11f7367bd2
dingjinhui <863208898@qq.com>
2019-12-07 10:08:19 +0800

    hystrix加入rabbitmq 可视化监控数据 hystrix结束

---
+++ microservice-hystrix-turbine-mq/src/main/java/com/itmcah/HystrixTurbineMqApplication.java
@@ -0,0 +1,13 @@
+package com.itmcah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableTurbineStream
+public class HystrixTurbineMqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineMqApplication.class, args);
+    }
+}
